9841e26ffe43852b0b2812d2e751462e6ebd06d6
fvasc <fvasc@github.com>
2018-01-11 13:33:01 -0200

    adicionando legenda

--- temp.py
+++ temp.py
@@ -9,8 +9,15 @@ import matplotlib.pyplot as plt
 
 x = [1,2,3]
 y = [5,7,4]
-plt.plot(x,y)
+
+x2 = [1,2,3]
+y2 = [10,14,12]
+
+
+plt.plot(x,y,label='Primeira linha')
+plt.plot(x2,y2,label='Segunda linha')
 plt.xlabel('Eixo X')
 plt.ylabel('Eixo Y')
 plt.title('Titulo do gráfico')
+plt.legend()
 plt.show()
