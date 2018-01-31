f746a1f745a35efc7c7fe7a23afc009b0aa31efc
fvasc <fvasc@github.com>
2018-01-31 19:48:06 -0200

    tarefa 1 finalizada

--- temp.py
+++ temp.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
 
-This is a temporary script file.
 """
+Felipe Vasconcelos
+8993027
+
+Tarefa #01
+
+Referencias: 
+    - https://pt.wikipedia.org/wiki/Equa%C3%A7%C3%A3o_de_Lotka-Volterra
+    - https://matplotlib.org/
+    - https://docs.python.org/3/
+"""
+
+
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
-import math
 
 
 #Classe CONSTRUTORDEGRAFICO
@@ -26,22 +34,20 @@ class ConstrutorDeGrafico:
             X = [float(linha.split()[1]) for linha in linhas]
             Y = [float(linha.split()[2]) for linha in linhas]
         ff.close
-
-        self.__configurarEixo(self.eixo1, 'Titulo do gráfico 1', T, X, tracejado, m, 'Eixo X')
-        self.__configurarEixo(self.eixo2, 'Titulo do gráfico 2', T, Y, tracejado, m, 'Eixo Y')        
+        self.__configurarEixo(self.eixo1, 'Qtd. de Presas (un.) X Tempo (t)', T, X, tracejado, m, 'Qtd. de Presas (un.)')
+        self.__configurarEixo(self.eixo2, 'Qtd. de Presas (un.) X Tempo (t)', T, Y, tracejado, m, 'Qtd. de Predadores (un.)')        
         
         
     def __configurarEixo(self, eixo, titulo, T, valores, tracejado, m, rotulo_y):
         eixo.set_title(titulo)
-        eixo.plot(T, valores, tracejado, label='n = ' + str(m), color="black")
+        eixo.plot(T, valores, tracejado, label='m = ' + str(m), color="black")
         eixo.set_ylabel(rotulo_y)
-        eixo.set_xlabel('Eixo T')        
+        eixo.set_xlabel('Tempo (t)')        
         eixo.legend()
 
 
     def mostrar(self):
         self.gs.tight_layout(self.fig)
-        
 #Fim da classe CONSTRUTORDEGRAFICO
 
 
@@ -51,12 +57,21 @@ class Simulador:
     
     def __init__(self, entrada):
         with open(entrada + '.txt') as f:
+            
             self.x0 = float(f.readline())
             self.y0 = float(f.readline())
+            
             self.t0 = float(f.readline())
             self.tf = float(f.readline())
+            
             self.n = int(f.readline())
             self.m = int(f.readline())
+            
+            self.alpha = float(f.readline())
+            self.beta = float(f.readline())
+            self.gama = float(f.readline())
+            self.delta = float(f.readline())
+        
         f.closed
         self.__simular()
         
@@ -75,7 +90,6 @@ class Simulador:
         Y = [self.y0]
 
         with open(self.nomeArquivoDeSaida(), 'w+') as f:    
-            
             for k in range(0, self.__calcularN()):
                 t_k1 = T[k] + h
                 x_k1 = X[k] + h * self.__fi(self.__eulerExplicito1(X[k], Y[k], T[k]))
@@ -84,7 +98,6 @@ class Simulador:
                 X.append(x_k1)
                 Y.append(y_k1)
                 f.write(str(t_k1) + ' ' + str(x_k1) + ' ' + str(y_k1) + '\n')
-            
         f.closed
             
             
@@ -97,12 +110,18 @@ class Simulador:
         
         
     def __eulerExplicito1(self, x, y, t):
-        return y
+        return self.__presa(x, y)
+    
+    def __presa(self, x, y):
+        return x * (self.alpha - self.beta * y)
     
     
     def __eulerExplicito2(self, x, y, t):
-        return -5*y + 4*x + math.sin(10*t)
+        return self.__predador(x, y)
         
+    def __predador(self, x, y):
+        return y * (self.delta * x - self.gama)
+    
     
     def nomeArquivoDeSaida(self):
         return 'saida_' + str(self.m) + '.txt'
@@ -112,9 +131,9 @@ class Simulador:
     
 def main():
     
-    sim2 = Simulador('entrada_2')
-    sim3 = Simulador('entrada_3')
-    sim4 = Simulador('entrada_4')
+    sim2 = Simulador('entrada_5')
+    sim3 = Simulador('entrada_6')
+    sim4 = Simulador('entrada_7')
 
     construtorDeGrafico = ConstrutorDeGrafico()    
     construtorDeGrafico.adicionar(sim2.nomeArquivoDeSaida(), sim2.m, '--')
