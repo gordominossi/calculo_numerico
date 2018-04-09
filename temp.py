27018aeb81e828ed63412e66e8fb3fe61bdd325e
fvasc <fvasc@github.com>
2018-04-09 18:29:44 -0300

    intercalando calculo dos ks de rk33

--- temp.py
+++ temp.py
@@ -29,10 +29,10 @@ class ConstrutorDeGrafico:
         with open(arquivo) as ff:
             linhas = ff.readlines()
             T = [float(linha.split()[0]) for linha in linhas]
-            Y1 = [float(linha.split()[1]) for linha in linhas]
-            Y2 = [float(linha.split()[2]) for linha in linhas]
-            Y3 = [float(linha.split()[3]) for linha in linhas]
-            Y4 = [float(linha.split()[4]) for linha in linhas]
+            Y1 = [float(linha.split()[1]) for linha in linhas] #S
+            Y2 = [float(linha.split()[2]) for linha in linhas] #I
+            Y3 = [float(linha.split()[3]) for linha in linhas] #R
+            Y4 = [float(linha.split()[4]) for linha in linhas] #A
         ff.close
         
         #TROCAR PARA IMPRIMIR UM GRAFICO POR VEZ. SINCRONIZAR COM SOLUCAO EXATA
@@ -71,11 +71,20 @@ class Simulador:
         with open('parametros_iniciais.txt') as f:
             self.t0 = float(f.readline())
             self.tf = float(f.readline())
-            self.y1_0 = float(f.readline())
-            self.y2_0 = float(f.readline())
-            self.y3_0 = float(f.readline())
-            self.y4_0 = float(f.readline())
+            self.y1_0 = float(f.readline()) #S
+            self.y2_0 = float(f.readline()) #I
+            self.y3_0 = float(f.readline()) #R
+            self.y4_0 = float(f.readline()) #A
             self.n = int(f.readline())
+            
+#            self.npc = int(f.readline()) #novos comp. adicionados
+#            self.tm = int(f.readline()) #taxa de mortalidade
+#            self.isi = int(f.readline()) #int. suscept. infect.
+#            self.isv = int(f.readline()) #int. suscept. vac.
+#            self.iiv = int(f.readline()) #int. infec. vac.
+#            self.cpc = int(f.readline()) #comp. consertados
+#            self.crm = int(f.readline()) #comp. removidos
+#            
         f.closed
         self.__simular()
 
@@ -102,20 +111,39 @@ class Simulador:
                t_k1 = tk + h
                T.append(t_k1)
 
-               y1_k = Y1[i] 
-               y2_k = Y2[i] 
-               y3_k = Y3[i] 
-               y4_k = Y4[i] 
+               y1_k = Y1[i] #S
+               y2_k = Y2[i] #I
+               y3_k = Y3[i] #R
+               y4_k = Y4[i] #A
                
-               y1_k1 = y1_k + self.__fi(self.__y1Linha, tk, h, y1_k, y2_k, y3_k, y4_k)
-               y2_k1 = y2_k + self.__fi(self.__y2Linha, tk, h, y1_k, y2_k, y3_k, y4_k)
-               y3_k1 = y3_k + self.__fi(self.__y3Linha, tk, h, y1_k, y2_k, y3_k, y4_k)
-               y4_k1 = y4_k + self.__fi(self.__y4Linha, tk, h, y1_k, y2_k, y3_k, y4_k)
+               #Calculando k1 do RK33
+               y1_rk1 = self.__RK33_k1(self.__y1Linha, tk, h, y1_k, y2_k, y3_k, y4_k) #S
+               y2_rk1 = self.__RK33_k1(self.__y2Linha, tk, h, y1_k, y2_k, y3_k, y4_k) #I
+               y3_rk1 = self.__RK33_k1(self.__y3Linha, tk, h, y1_k, y2_k, y3_k, y4_k) #R
+               y4_rk1 = self.__RK33_k1(self.__y4Linha, tk, h, y1_k, y2_k, y3_k, y4_k) #A
+
+               #Calculando k2 do RK33               
+               y1_rk2 = self.__RK33_k2(self.__y1Linha, tk, h, y1_k, y2_k, y3_k, y4_k, y1_rk1, y2_rk1, y3_rk1, y4_rk1) #S
+               y2_rk2 = self.__RK33_k2(self.__y2Linha, tk, h, y1_k, y2_k, y3_k, y4_k, y1_rk1, y2_rk1, y3_rk1, y4_rk1) #I
+               y3_rk2 = self.__RK33_k2(self.__y3Linha, tk, h, y1_k, y2_k, y3_k, y4_k, y1_rk1, y2_rk1, y3_rk1, y4_rk1) #R
+               y4_rk2 = self.__RK33_k2(self.__y4Linha, tk, h, y1_k, y2_k, y3_k, y4_k, y1_rk1, y2_rk1, y3_rk1, y4_rk1) #A
                
-               Y1.append(y1_k1)
-               Y2.append(y2_k1)
-               Y3.append(y3_k1)
-               Y4.append(y4_k1)
+               #Calculando k3 do RK33               
+               y1_rk3 = self.__RK33_k3(self.__y1Linha, tk, h, y1_k, y2_k, y3_k, y4_k, y1_rk1, y2_rk1, y3_rk1, y4_rk1, y1_rk2, y2_rk2, y3_rk2, y4_rk2) #S
+               y2_rk3 = self.__RK33_k3(self.__y2Linha, tk, h, y1_k, y2_k, y3_k, y4_k, y1_rk1, y2_rk1, y3_rk1, y4_rk1, y1_rk2, y2_rk2, y3_rk2, y4_rk2) #I
+               y3_rk3 = self.__RK33_k3(self.__y3Linha, tk, h, y1_k, y2_k, y3_k, y4_k, y1_rk1, y2_rk1, y3_rk1, y4_rk1, y1_rk2, y2_rk2, y3_rk2, y4_rk2) #R
+               y4_rk3 = self.__RK33_k3(self.__y4Linha, tk, h, y1_k, y2_k, y3_k, y4_k, y1_rk1, y2_rk1, y3_rk1, y4_rk1, y1_rk2, y2_rk2, y3_rk2, y4_rk2) #A
+               
+               
+               y1_k1 = y1_k + self.__RK33(y1_rk1, y1_rk2, y1_rk3) #S
+               y2_k1 = y2_k + self.__RK33(y2_rk1, y2_rk2, y2_rk3) #I
+               y3_k1 = y3_k + self.__RK33(y3_rk1, y3_rk2, y3_rk3) #R
+               y4_k1 = y4_k + self.__RK33(y4_rk1, y4_rk2, y4_rk3) #A
+               
+               Y1.append(y1_k1) #S
+               Y2.append(y2_k1) #I
+               Y3.append(y3_k1) #R
+               Y4.append(y4_k1) #A
                
                f.write(str(t_k1) + ' ' + str(y1_k1) + ' ' + str(y2_k1) + ' ' + str(y3_k1) + ' ' + str(y4_k1) + '\n')
               
@@ -127,19 +155,46 @@ class Simulador:
     def nomeArquivoDeSaida(self):
         return 'saida_' + str(self.m) + '.txt'
 
-    def __fi(self, ylinha, tk, h, y1_k, y2_k, y3_k, y4_k):
-        return self.__rungeKuttaTerceiraOrdem(ylinha, tk, h, y1_k, y2_k, y3_k, y4_k)
 
-    def __rungeKuttaTerceiraOrdem(self, ylinha, tk, h, y1_k, y2_k, y3_k, y4_k):
+    #RUNGE-KUTTA 3 ORDEM
+    def __RK33_k1(self, ylinha, tk, h, y1_k, y2_k, y3_k, y4_k):
         k1 = h * ylinha(tk, y1_k, y2_k, y3_k, y4_k)
-        
-        k2_fator = (k1/2)
-        k2 = h * ylinha(tk + h/2, y1_k + k2_fator, y2_k + k2_fator, y3_k + k2_fator, y4_k + k2_fator)
-        
-        k3_fator = - k1 + (2*k2)
-        k3 = h * ylinha(tk + h, y1_k + k3_fator, y2_k + k3_fator, y3_k + k3_fator, y4_k + k3_fator)
-        
+        return k1
+    
+    def __RK33_k2(self, ylinha, tk, h, y1_k, y2_k, y3_k, y4_k, y1_rk1, y2_rk1, y3_rk1, y4_rk1):
+        y1_rk2_fator = (y1_rk1/2)
+        y2_rk2_fator = (y2_rk1/2)
+        y3_rk2_fator = (y3_rk1/2)
+        y4_rk2_fator = (y4_rk1/2)
+        k2 = h * ylinha(tk + h/2, y1_k + y1_rk2_fator, y2_k + y2_rk2_fator, y3_k + y3_rk2_fator, y4_k + y4_rk2_fator)
+        return k2
+    
+    def __RK33_k3(self, ylinha, tk, h, y1_k, y2_k, y3_k, y4_k, y1_rk1, y2_rk1, y3_rk1, y4_rk1, y1_rk2, y2_rk2, y3_rk2, y4_rk2):
+        y1_rk3_fator = - y1_rk1 + (2*y1_rk2)
+        y2_rk3_fator = - y2_rk1 + (2*y2_rk2)
+        y3_rk3_fator = - y3_rk1 + (2*y3_rk2)
+        y4_rk3_fator = - y4_rk1 + (2*y4_rk2)
+        k3 = h * ylinha(tk + h, y1_k + y1_rk3_fator, y2_k + y2_rk3_fator, y3_k + y3_rk3_fator, y4_k + y4_rk3_fator)
+        return k3
+    
+    def __RK33(self, k1, k2, k3):
         return (k1 + 4 * k2 + k3)/6
+    #FIM do RUNGE-KUTTA 3 ORDEM
+    
+    
+#    def __fi(self, ylinha, tk, h, y1_k, y2_k, y3_k, y4_k):
+#        return self.__rungeKuttaTerceiraOrdem(ylinha, tk, h, y1_k, y2_k, y3_k, y4_k)
+#    
+#    def __rungeKuttaTerceiraOrdem(self, ylinha, tk, h, y1_k, y2_k, y3_k, y4_k):
+#        k1 = h * ylinha(tk, y1_k, y2_k, y3_k, y4_k)
+#        
+#        k2_fator = (k1/2)
+#        k2 = h * ylinha(tk + h/2, y1_k + k2_fator, y2_k + k2_fator, y3_k + k2_fator, y4_k + k2_fator)
+#        
+#        k3_fator = - k1 + (2*k2)
+#        k3 = h * ylinha(tk + h, y1_k + k3_fator, y2_k + k3_fator, y3_k + k3_fator, y4_k + k3_fator)
+#        
+#        return (k1 + 4 * k2 + k3)/6
     
     def __y1Linha(self, tk, y1_k, y2_k, y3_k, y4_k):
         return 1 * y1_k
@@ -172,10 +227,10 @@ class GeradorDeSolucaoExata:
 
             for k in range(0, self.i+1):
                 t_k1 = t + h * k
-                y_k1 = self.__calcularSolucaoExata1(t_k1)
-#                y_k1 = self.__calcularSolucaoExata2(t_k1)
-#                y_k1 = self.__calcularSolucaoExata3(t_k1)
-#                y_k1 = self.__calcularSolucaoExata4(t_k1)
+                y_k1 = self.__calcularSolucaoExata1(t_k1) #S
+#                y_k1 = self.__calcularSolucaoExata2(t_k1) #I
+#                y_k1 = self.__calcularSolucaoExata3(t_k1) #R
+#                y_k1 = self.__calcularSolucaoExata4(t_k1) #A
 
                 f.write(str(t_k1) + ' ' + str(y_k1) + '\n')
         f.closed
